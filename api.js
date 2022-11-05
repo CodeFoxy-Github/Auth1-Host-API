2833e40cd174b8de0979241029df4084ec33687d
CodeFoxy-Github <96522178+CodeFoxy-Github@users.noreply.github.com>
2022-11-05 10:45:20 +0800

    1 file modified

--- api.js
+++ api.js
@@ -6,6 +6,15 @@ const g = 'google';
 const f = 'facebook';
 const lf = 'login_with';
 const cf = 'continue_with';
+console.log('%cWARNING', 'font-size:10em;color:red;');
+console.log(
+  `%cThis is a browser feature intended for developers.
+Do NOT copy and paste something here if you do not understand it.
+
+You can learn more at:
+https://en.wikipedia.org/wiki/Self-XSS`,
+  'font-size:2em'
+);
 if (application == g) {
   console.log('Application Found! (Google)');
 } else if (application == f) {
@@ -15,7 +24,7 @@ if (application == g) {
   } else if (buttontext == lf) {
     console.log('Facebook: Button-text = login_with');
     const code1 =
-      ('\<div class\=\"fb-login-button\" data-width=\"\" data-size\=\"large\"data-button-type=\"continue_with\" data-layout=\"default\"data-auto-logout-link=\"false\" data-use-continue-as=\"false\"\>\<\/div\>');
+      '<div class="fb-login-button" data-width="" data-size="large"data-button-type="continue_with" data-layout="default"data-auto-logout-link="false" data-use-continue-as="false"></div>';
     console.log(code1);
   } else {
     console.log('N/A Button-text No Found! (' + buttontext + ')');
